05ab1cc863f2604af26b3addb0fd859cbfd5f1f6
BrunoValsecchi <cai.bruno.valsecchi@gmail.com>
2021-09-29 16:55:37 -0300

    algoritmos condicionales

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+let precio = prompt ("ingrese un numero")
+let precio2 = 70
+
+if(precio <= precio2) {
+    alert ("su numero es menor o igual 70")
+}
+
+else{ 
+    alert("Su numero es mayor que 70")
+}
+
+let nombreUsuario = prompt("Ingrese su nombre");
+
+
+if((nombreUsuario !="" ) && ((nombreUsuario == "Juan") || (nombreUsuario == "juan"))){
+    alert("Hola Juan")
+}
+else {
+    alert("Ha ingresado mal su nombre")
+}
